765cd8e800bdcdef26c7bd20d18d1e85e3e72ff8
kwan2gwat1gwai1 <81792917+kwan2gwat1gwai1@users.noreply.github.com>
2021-04-07 11:28:54 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,16 +3,16 @@
  */
 //%
 enum GamerBitPin {
-    //% block="D-PAD up"
+    //% block="G-PAD up"
     P15 = DAL.MICROBIT_ID_IO_P15,
-    //% block="D-PAD down"
+    //% block="G-PAD down"
     P13 = DAL.MICROBIT_ID_IO_P13,
-    //% block="D-PAD left"
+    //% block="G-PAD left"
     P16 = DAL.MICROBIT_ID_IO_P16,
-    //% block="D-PAD right"
+    //% block="G-PAD right"
     P14 = DAL.MICROBIT_ID_IO_P14,
 
-    //% block="D-PAD joystick"
+    //% block="G-PAD joystick"
     P8 = DAL.MICROBIT_ID_IO_P8,
 }
 
